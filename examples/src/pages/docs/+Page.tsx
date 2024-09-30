ac0a7907b67393e67f4d70c50fe96b9b8c00b2be
Stu Kabakoff <sakabako@gmail.com>
2024-09-30 14:44:14 -0400

    got it working with markdown

---
+++ examples/src/pages/docs/+Page.tsx
@@ -0,0 +1,10 @@
+import Readme from '../../../../README.md';
+
+export const Page = () => {
+    return (
+        <div>
+            <h1>Documentation</h1>
+            <Readme />
+        </div>
+    );
+};
