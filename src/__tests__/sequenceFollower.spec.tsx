e2266680aabb40d9368f16d7aa1e2f8d64a3612a
Stu Kabakoff <sakabako@gmail.com>
2024-10-08 09:43:52 -0400

    add sequenceFollower testing util, release action

---
+++ src/__tests__/sequenceFollower.spec.tsx
@@ -0,0 +1,241 @@
+// tests/followSequence.test.js
+
+import React from 'react';
+import { followSequence } from '../testUtils/followSequence';
+import type { SequenceChild } from '../types';
+
+describe('followSequence', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should return all required pages in order for a simple linear sequence', () => {
+        const pages = [
+            {
+                id: 'page1',
+                isComplete: () => false,
+                Component: () => <div />,
+            },
+            {
+                id: 'page2',
+                isComplete: () => false,
+                Component: () => <div />,
+            },
+            {
+                id: 'page3',
+                isComplete: () => false,
+                Component: () => <div />,
+            },
+        ];
+
+        const data = {};
+
+        const visitedPages = followSequence(pages, data);
+
+        expect(visitedPages.map((page) => page.id)).toEqual([
+            'page1',
+            'page2',
+            'page3',
+        ]);
+    });
+
+    it('should skip pages where isRequired returns false', () => {
+        const pages = [
+            {
+                id: 'page1',
+                isRequired: () => true,
+                isComplete: () => false,
+                Component: () => <div />,
+            },
+            {
+                id: 'page2',
+                isRequired: () => false,
+                isComplete: () => false,
+                Component: () => <div />,
+            },
+            {
+                id: 'page3',
+                isRequired: () => true,
+                isComplete: () => false,
+                Component: () => <div />,
+            },
+        ];
+
+        const data = {};
+
+        const visitedPages = followSequence(pages, data);
+
+        expect(visitedPages.map((page) => page.id)).toEqual(['page1', 'page3']);
+    });
+
+    it('should navigate to alternate next page when alternateNextPage returns a valid page ID', () => {
+        const pages = [
+            {
+                id: 'page1',
+                alternateNextPage: () => 'page3',
+                isComplete: () => false,
+                Component: () => <div />,
+            },
+            { id: 'page2', isComplete: () => false, Component: () => <div /> },
+            { id: 'page3', isComplete: () => false, Component: () => <div /> },
+            { id: 'page4', isComplete: () => false, Component: () => <div /> },
+        ];
+
+        const data = {};
+
+        const visitedPages = followSequence(pages, data);
+
+        expect(visitedPages.map((page) => page.id)).toEqual([
+            'page1',
+            'page3',
+            'page4',
+        ]);
+    });
+
+    it('should stop navigation when a page with isFinal returns true', () => {
+        const pages = [
+            { id: 'page1', isComplete: () => false, Component: () => <div /> },
+            {
+                id: 'page2',
+                isFinal: () => true,
+                isComplete: () => false,
+                Component: () => <div />,
+            },
+            { id: 'page3', isComplete: () => false, Component: () => <div /> },
+        ];
+
+        const data = {};
+
+        const visitedPages = followSequence(pages, data);
+
+        expect(visitedPages.map((page) => page.id)).toEqual(['page1', 'page2']);
+    });
+
+    it('should proceed to next page when alternateNextPage returns an invalid page ID', () => {
+        console.warn = jest.fn();
+
+        const pages = [
+            {
+                id: 'page1',
+                alternateNextPage: () => 'invalidPage',
+                isComplete: () => false,
+                Component: () => <div />,
+            },
+            { id: 'page2', isComplete: () => false, Component: () => <div /> },
+            { id: 'page3', isComplete: () => false, Component: () => <div /> },
+        ];
+
+        const data = {};
+
+        const visitedPages = followSequence(pages, data);
+
+        expect(visitedPages.map((page) => page.id)).toEqual([
+            'page1',
+            'page2',
+            'page3',
+        ]);
+    });
+
+    it('should handle data-dependent isRequired and alternateNextPage functions', () => {
+        const formData = { includePage1: true, skipToPage4: true };
+
+        const pages = [
+            {
+                id: 'page1',
+                isRequired: (data: Partial<typeof formData>) =>
+                    data.includePage1,
+                isComplete: () => false,
+                Component: () => <div />,
+            },
+            {
+                id: 'page2',
+                alternateNextPage: (data: Partial<typeof formData>) =>
+                    data.skipToPage4 ? 'page4' : undefined,
+                isComplete: () => false,
+                Component: () => <div />,
+            },
+            { id: 'page3', isComplete: () => false, Component: () => <div /> },
+            { id: 'page4', isComplete: () => false, Component: () => <div /> },
+        ];
+
+        const visitedPages = followSequence(pages, formData);
+
+        expect(visitedPages.map((page) => page.id)).toEqual([
+            'page1',
+            'page2',
+            'page4',
+        ]);
+    });
+
+    it('should handle an empty sequence', () => {
+        const pages: SequenceChild<any, any, any>[] = [];
+
+        const data = {};
+
+        const visitedPages = followSequence(pages, data);
+
+        expect(visitedPages).toEqual([]);
+    });
+
+    it('should prevent infinite loops when alternateNextPage creates a loop', () => {
+        const pages = [
+            {
+                id: 'page1',
+                alternateNextPage: () => 'page2',
+                isComplete: () => false,
+                Component: () => <div />,
+            },
+            {
+                id: 'page2',
+                alternateNextPage: () => 'page1',
+                isComplete: () => false,
+                Component: () => <div />,
+            },
+            { id: 'page3', isComplete: () => false, Component: () => <div /> },
+        ];
+
+        const data = {};
+
+        expect(() =>
+            followSequence(pages, data),
+        ).toThrowErrorMatchingInlineSnapshot(
+            `"Loop detected at page 'page1'. Navigation stopped. Full path: page1 -> page2 -> page1"`,
+        );
+    });
+
+    it('should handle pages with isFinal depending on data', () => {
+        const formData = { endHere: true };
+
+        const pages = [
+            {
+                id: 'page1',
+                isFinal: (data: Partial<typeof formData>) => !!data.endHere,
+                isComplete: () => false,
+                Component: () => <div />,
+            },
+            { id: 'page2', isComplete: () => false, Component: () => <div /> },
+        ];
+
+        const visitedPages = followSequence(pages, formData);
+
+        expect(visitedPages.map((page) => page.id)).toEqual(['page1']);
+    });
+
+    it('should continue navigation when isFinal returns false', () => {
+        const pages = [
+            {
+                id: 'page1',
+                isFinal: () => false,
+                isComplete: () => false,
+                Component: () => <div />,
+            },
+            { id: 'page2', isComplete: () => false, Component: () => <div /> },
+        ];
+
+        const data = {};
+
+        const visitedPages = followSequence(pages, data);
+
+        expect(visitedPages.map((page) => page.id)).toEqual(['page1', 'page2']);
+    });
+});
---
+++ src/testUtils/followSequence.ts
@@ -0,0 +1,69 @@
+import type { FormPage, FormSequence, SequenceChild } from '../types';
+import { flattenPages } from '../utils';
+
+/**
+ * Follows a sequence of pages based on the provided data.
+ * This function will return an array of pages that were visited in order.
+ *
+ * @param {SequenceChild[]} sequence - The sequence of pages to follow.
+ * @param {DataT} data - The data to use when determining which pages to visit.
+ * @returns {FormPage[]} An array of pages that were visited in order.
+ */
+export function followSequence<DataT, U, V>(
+    sequence: SequenceChild<DataT, U, V>[] | FormSequence<DataT, U, V>,
+    data: DataT,
+): FormPage<DataT, U, V>[] {
+    if (!Array.isArray(sequence)) {
+        sequence = sequence.pages;
+    }
+    const [pages, pageMap] = flattenPages(sequence);
+
+    const visitedPages: FormPage<DataT, any, any>[] = [];
+    const visitedPageIds = new Set<string>();
+
+    // start at the first page
+    // for each page
+    // if it's required, add it to the visited pages
+    // if it's not required, skip it
+    // if it has a alternateNextPage, go to that page
+    // continue until the end is reached or a page has isFinal
+    let currentPage: FormPage<DataT, U, V> | undefined = pages[0];
+    while (currentPage) {
+        visitedPages.push(pageMap[currentPage.id]);
+
+        if (visitedPageIds.has(currentPage.id)) {
+            throw new Error(
+                `Loop detected at page '${currentPage.id}'. Navigation stopped. Full path: ${visitedPages.map((p) => p.id).join(' -> ')}`,
+            );
+        }
+        visitedPageIds.add(currentPage.id);
+
+        if (currentPage.isFinal?.(data)) {
+            break;
+        }
+
+        const alternateNextPage: string | any =
+            currentPage.alternateNextPage?.(data);
+        if (alternateNextPage) {
+            const nextPage = pages.find(
+                (page) => page.id === alternateNextPage,
+            );
+            if (nextPage) {
+                currentPage = nextPage;
+                continue;
+            }
+        }
+
+        const searchStartIndex: number = pages.indexOf(currentPage) + 1;
+        currentPage = undefined;
+        for (let i = searchStartIndex; i < pages.length; i++) {
+            const nextPage = pages[i];
+            if (!nextPage.isRequired || nextPage.isRequired(data)) {
+                currentPage = nextPage;
+                break;
+            }
+        }
+    }
+
+    return visitedPages;
+}
